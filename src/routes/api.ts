35eb97989e342b8dff121e6cd09d35fa919c1a66
unknown <reveliowalker22@gmail.com>
2026-05-21 20:41:27 +0700

    initial commit

---
+++ src/routes/api.ts
@@ -0,0 +1,11 @@
+import e from 'express';
+import exprees from 'express';
+
+const router = exprees.Router();
+
+// router.get('/dummy', (req: e.Request, res: e.Response) => {
+//   res.json({ message: 'Hello, World!' });
+// });
+
+export default router;
+
